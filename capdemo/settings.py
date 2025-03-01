81c488de3599a1390f0275164dfb1d0b374dc9e7
kavish54 <114338956+kavish54@users.noreply.github.com>
2025-03-01 15:17:34 +0530

    Update settings.py

--- capdemo/settings.py
+++ capdemo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-opl=ovd2@b4^*9o-(2^aaethy#b2r195h5a!%cr_#4o)q!dqd7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
